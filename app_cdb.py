bb10a196d745a5e8f91661376ba4add21356999d
joytafty <joytafty@gmail.com>
2013-11-13 23:42:21 -0500

    remove junk from app_cdb.py

--- app_cdb.py
+++ app_cdb.py
@@ -119,7 +119,7 @@ def authenticate():
 def server():
     from cherrypy import wsgiserver
     app = Flask(__name__)
-    app.config.from_object('config')
+    # app.config.from_object('config')
 
     @app.route("/")
     def index():
